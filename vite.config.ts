c7847eb64663881be2d330df0b3dcaea7f4afbe6
Brett Schellenberg <brett.schellenberg@gmail.com>
2023-09-04 13:48:11 -0600

    bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
+// import {visualizer} from 'rollup-plugin-visualizer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "", // relative links, for gh pages
   plugins: [
+    // visualizer({
+    //   template: "treemap", // or sunburst
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    //   filename: "analyse.html", // will be saved in project's root
+    // }) as PluginOption,
     react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
   ],
   css: {
